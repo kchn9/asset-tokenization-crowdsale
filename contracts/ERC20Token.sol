0c9098f38ee6a19e5c9ddb1a672156b78e42111a
kchn9 <mat.kuchnia@gmail.com>
2022-04-26 15:07:31 +0200

    Create ERC20 fixed supply token

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC20Token {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract ERC20Token is ERC20 {
+    constructor(string memory _name, string memory _symbol, uint _fixedSupply) ERC20(_name, _symbol) {
+        _mint(msg.sender, _fixedSupply);
+    }
 }
