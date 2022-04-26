b57679bf55b99708d65a613ced7d6998bab9607a
kchn9 <mat.kuchnia@gmail.com>
2022-04-26 18:33:08 +0200

    Change deploy name

---
+++ migrations/2_deploy_token.js
@@ -0,0 +1,8 @@
+require("dotenv").config({ path: "../.env" });
+
+const ERC20Token = artifacts.require("./ERC20Token.sol");
+
+module.exports = function(deployer, _, accounts) {
+  const [ tokenDeployer, recepient, ...rest ] = accounts;
+  deployer.deploy(ERC20Token, process.env.NAME, process.env.SYMBOL, process.env.TOTAL_SUPPLY, { from: tokenDeployer });
+};
