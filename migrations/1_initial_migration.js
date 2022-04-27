1952979ccc8611ba2893b5b4d4a7b95aae61ff1c
kchn9 <mat.kuchnia@gmail.com>
2022-04-27 14:33:33 +0200

    Add dotenv comment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,5 @@
-require("dotenv").config({ path: "../.env" });
+// import dotenv to get access to {process.env.*} variables
+require("dotenv").config({ path: "../.env" }); 
 
 const Migrations = artifacts.require("./Migrations.sol");
 module.exports = function(deployer) {
