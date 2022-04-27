772053819f3ba3c1593f2216cc347af5d574f50f
kchn9 <mat.kuchnia@gmail.com>
2022-04-27 17:20:18 +0200

    truffle migrate --reset

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -2,5 +2,13 @@ const ERC20Token = artifacts.require("./ERC20Token.sol");
 
 module.exports = function(deployer, _, accounts) {
   const [ tokenDeployer, recepient, ...rest ] = accounts;
-  deployer.deploy(ERC20Token, process.env.NAME, process.env.SYMBOL, process.env.TOTAL_SUPPLY, { from: tokenDeployer });
+  deployer.then(async function() {
+    try {
+      await deployer.deploy(ERC20Token, process.env.NAME, process.env.SYMBOL, process.env.TOTAL_SUPPLY, { from: tokenDeployer });
+      const instance = await ERC20Token.deployed();
+      await instance.mint(process.env.APPROVED_TO_SELL, tokenDeployer, { from: tokenDeployer });
+    } catch (error) {
+      console.log(error);
+    }
+  })
 };
