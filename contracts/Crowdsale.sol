c69f0129730139be0b167e3a41212333df939577
kchn9 <mat.kuchnia@gmail.com>
2022-04-27 17:41:12 +0200

    Add buyToken()

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -6,11 +6,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Crowdsale is Ownable {
 
-    ERC20 immutable token;
-    address payable immutable seller;
+    ERC20 immutable internal _ERC20token;
+    address payable immutable public recipient;
 
-    constructor(ERC20 _token, address payable _seller) {
-        token = _token;
-        seller = _seller;
+    constructor(ERC20 _token, address payable _recipient) {
+        require(_token.balanceOf(owner()) > 0, "Crowdsale: Deployer has no tokens to sell.");
+        require(_recipient != address(0), "Crowdsale: Receipient cannot be address 0.");
+        _ERC20token = _token;
+        recipient = _recipient;
+    }
+
+    function buyTokens(uint _amount) external {
+        require(_ERC20token.allowance(owner(), address(this)) > 0, "Crowdsale: Contract has no rights to sell tokens on owners behalf");
+        require(getLeftAllowance() >= _amount, "Crowdsale: Amount exceeds left allowance.");
+        _ERC20token.transferFrom(owner(), msg.sender, _amount);
+    }
+
+    function getLeftAllowance() public view returns(uint) {
+        return _ERC20token.allowance(owner(), address(this));
     }
 }
