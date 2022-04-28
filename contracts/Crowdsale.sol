a352551b55405b2645b41d9b4c241f9d68a237c4
kchn9 <mat.kuchnia@gmail.com>
2022-04-28 11:11:36 +0200

    Add KYC validation before buyTokens()

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -3,29 +3,38 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./Pausable.sol";
+import "./KYCCheck.sol";
 
 contract Crowdsale is Pausable {
 
     event TokensPurchased(address who, uint amount);
 
-    ERC20 immutable internal _ERC20token;
+    ERC20 immutable private _ERC20token;
+    KYCCheck immutable private _KYCCheck;
+    
     address payable immutable public recipient;
     uint public immutable rate; // tokens for 1 eth
 
-    constructor(ERC20 _token, address payable _recipient, uint _rate) Pausable() {
+    constructor(ERC20 _token, KYCCheck _KYCContract,  address payable _recipient, uint _rate) Pausable() {
         require(_token.balanceOf(owner()) > 0, "Crowdsale: Deployer has no tokens to sell.");
         require(_recipient != address(0), "Crowdsale: Receipient cannot be address 0.");
         _ERC20token = _token;
+        _KYCCheck = KYCCheck(_KYCContract);
         recipient = _recipient;
         rate = _rate;
     }
 
     function buyTokens() public payable notPaused {
+        require(_KYCCheck.isAllowed(msg.sender), "Crowdsale: Caller KYC is not completed yet.");
+
         uint tokenAmount = (msg.value * rate) / 10 ** _ERC20token.decimals();
+        require(tokenAmount != 0, "Crowdsale: Token amount cannot be 0.");
         require(getLeftAllowance() >= tokenAmount, "Crowdsale: Amount exceeds left allowance.");
+
+        _ERC20token.transferFrom(owner(), msg.sender, tokenAmount);
         (bool success, /* data */) = recipient.call{ value: msg.value }("");
         require(success, "Crowdsale: ETH transfer failed.");
-        _ERC20token.transferFrom(owner(), msg.sender, tokenAmount);
+        
         emit TokensPurchased(msg.sender, tokenAmount);
     }
 
